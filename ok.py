693993324be74a64d9e9c3bee2b67161bf0bf806
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2020-11-13 01:09:11 +0530

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,72 @@
+def choose_file_for_read():
+    filenam = open('C:/Users/PC/AppData/Local/Programs/Microsoft VS Cod/.vscode/work/file/list_of_list.txt', 'r')
+    gkmk = filenam.readlines()
+
+    list_box  = []
+    print('/n')
+    i = 1
+    for r in gkmk:
+        print(i, r.strip())
+        # append data with strip method to remov /n
+        list_box.append(r.strip())
+        i += 1
+    
+    option = input('/nEnter your options: ')
+    print('/nOption: ', option)
+    print('/nFinal list: ', list_box)
+
+    user_options()
+    filenam.close
+
+# choose_to_write_in_file()
+def choose_to_write_in_file():
+    filename = input('/nEnter file name: ')
+
+    # open file
+    myfile = open('C:/Users/PC/AppData/Local/Programs/Microsoft VS Code/.vscode/work/file/'+filename+'.txt', 'a')
+
+    # add data on this file
+    user_message = input('Enter your message: ')
+
+    # add message to file
+    myfile.write(user_message)
+    print('/nYour message is added on ' + filename + '.txt  file.')
+
+    # finally close the file
+    myfile.close()
+
+    user_options()
+
+# Provide option to user for read and write:
+
+def user_options():
+    message = '''
+    ------------------------------------------
+                Hello User
+    ------------------------------------------
+    Choose available options:
+    
+    1. Choose File for Read
+    2. Choose to write in available file
+    3. Exit
+    -------------------------------------------
+    '''
+   
+
+    print(message)
+
+    try:
+        options = int(input('Enter numbre to visist available options: '))
+    except:
+        print('/nOnly numbers allowed. Try again with number.')
+    else:
+        if options == 1:
+            choose_file_for_read()
+        elif options == 2:
+             choose_to_write_in_file()
+        elif options == 3:
+            print('/nYou are successfully exit.')
+        else:
+            print('/nYou entered wrong options. Try again.')
+
+user_options()
